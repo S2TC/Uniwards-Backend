0629b9dad64dfd1c4d97b169ae83dd9ddbb56373
John Hopkin <umayr.sheik@gmail.com>
2018-05-03 01:08:09 +0400

    Legit ima kill myself

--- StudentHandle.py
+++ StudentHandle.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '*EtG*J 8);lJzP`HF}S5_v>aFLHX6D>qu)~&q5xF+rY{Fqixz,5A#h]M`Q%?+?gG'
 def GetStudent(token):
     payload = jwt.decode(token, app.config['SECRET_KEY'])
-    temp_user = SQLHandle.student.query.filter_by(username=payload['username'])
+    temp_user = SQLHandle.student.query.filter_by(username=payload['username']).first()
     if(temp_user is not None):
         temp_user.password = ""
         response = ResponseHandle.GenerateStudentResponse("student_get_success", temp_user.todict())
